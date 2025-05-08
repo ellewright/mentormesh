f504084b5a9842b23c24b00681ad4ef2d71c3e9c
Eleanor Wright <elliewright055@gmail.com>
2025-05-08 14:24:39 -0500

    Add get_skill_by_id(skill_id) endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from http.client import HTTPException
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -25,5 +26,12 @@ skills = {
 }
 
 @app.get("/")
-def index() -> dict[str, dict[int, Skill]]:
+def get_all_skills() -> dict[str, dict[int, Skill]]:
     return {"skills": skills}
+
+@app.get("/skills/{skill_id}")
+def get_skill_by_id(skill_id: int) -> Skill:
+    if skill_id not in skills:
+        raise HTTPException(status_code=404, detail=f"Could not find skill with id {skill_id}.")
+    
+    return skills[skill_id]
