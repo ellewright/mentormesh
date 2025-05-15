986fe491c625557afe1a7a3e779cd1e1f97895f3
Eleanor Wright <elliewright055@gmail.com>
2025-05-15 15:12:14 -0500

    Create Skill repository class

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,14 @@
 import sys
+
+sys.path.append("./model")
+sys.path.append("./repository")
+
+from skill import Skill
+from skill_repository import skills
 from http.client import HTTPException
 from fastapi import FastAPI
 
-sys.path.append("./model")
-
-from skill import Skill
-from skill import Category
-
 app = FastAPI()
-
-skills = {
-    0: Skill(id=0, category=Category.LIFE, title="How to Change a Tire", description="I'll teach you how to change a car tire."),
-    1: Skill(id=1, category=Category.MUSIC, title="How to Play Free Bird Solo", description="I'll teach you how to play the Free Bird solo on guitar."),
-    2: Skill(id=2, category=Category.MATH, title="The Pythagorean Theorem", description="I'll teach you the Pythagorean theorem so it's easy to understand.")
-}
 
 @app.get("/")
 def get_all_skills() -> dict[str, dict[int, Skill]]:
