7bbd26b586d4d83a8362bfdcacc3829b2f095425
Eleanor Wright <elliewright055@gmail.com>
2025-05-19 15:31:51 -0500

    Create router

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,27 @@
 import styles from "./NavBar.module.css"
+import { Outlet, useNavigation } from "react-router-dom"
 
 export default function NavBar() {
     return (
         <div className={styles.navbar}>
             <h1><span className={styles.header}>MentorMesh</span>: <span className={styles.slogan}>A Peer-to-Peer Learning Platform</span></h1>
         </div>
+    )
+}
+
+export function NavLayout() {
+    const { state } = useNavigation()
+    const isLoading = state === "loading"
+
+    return (
+        <>
+            <NavBar />
+            {isLoading &&
+                <div className={styles.spinner} />
+            }
+            <div className={styles.container + (isLoading ? "loading" : "")}>
+                <Outlet />
+            </div>
+        </>
     )
 }
---
+++ frontend/src/router/router.jsx
@@ -0,0 +1,43 @@
+import { useRouteError } from "react-router-dom";
+import { NavLayout } from "../components/NavBar/NavBar";
+import SkillsPage from "../pages/SkillsPage/SkillsPage";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <NavLayout />,
+        children: [
+            {
+                errorElement: <ErrorPage />,
+                children: [
+                    {
+                        path: "/skills",
+                        children: [
+                            {
+                                index: true,
+                                element: <SkillsPage />
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+])
+
+function ErrorPage() {
+    const error = useRouteError()
+
+    return (
+        <>
+            <h1>Something went wrong!</h1>
+            {import.meta.env.MODE != "production" && (
+                <>
+                    <pre>{error.message}</pre>
+                    <pre>{error.stack}</pre>
+                </>
+            )}
+        </>
+    )
+}
