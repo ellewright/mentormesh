4c8c0f19769ad9bfe5166a5aae2fc0f434495bd9
Eleanor Wright <elliewright055@gmail.com>
2025-05-20 13:44:47 -0500

    Style SkillCard

--- frontend/src/components/SkillCard/SkillCard.jsx
+++ frontend/src/components/SkillCard/SkillCard.jsx
@@ -5,7 +5,7 @@ import { getAllSkills } from "../../api/config"
 export default function SkillCard({ title, description, category }) {
     return (
         <div className={styles.card}>
-            <p className={styles.category}>{category}</p>
+            <p className={styles.category}>{category.toUpperCase()}</p>
             <h1 className={styles.title}>{title}</h1>
             <p className={styles.description}>{description}</p>
         </div>
