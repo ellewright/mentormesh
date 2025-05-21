e743a95a34432aca1c386a4e86fc5cbf3898476d
Eleanor Wright <elliewright055@gmail.com>
2025-05-21 15:21:38 -0500

    Add more dummy skills for testing

--- backend/repository/skill_repository.py
+++ backend/repository/skill_repository.py
@@ -11,5 +11,7 @@ skills = [
     Skill(id=2, category=Category.MATH, title="The Pythagorean Theorem", description="I'll teach you the Pythagorean theorem so it's easy to understand."),
     Skill(id=3, category=Category.SCIENCE, title="Explaining Evolution", description="Learn evolution from a certified biologist."),
     Skill(id=4, category=Category.HISTORY, title="Life in the Middle Ages", description="Find out what life was like in 600 AD."),
-    Skill(id=5, category=Category.LIFE, title="Need Tax Help?", description="I'll file your taxes!")
+    Skill(id=5, category=Category.LIFE, title="Need Tax Help?", description="I'll file your taxes!"),
+    Skill(id=6, category=Category.MATH, title="Calculus 101", description="I'll give you a tutor lesson for Calculus 101."),
+    Skill(id=7, category=Category.MUSIC, title="Voice lesson", description="Get a voice lesson from a professional Broadway singer.")
 ]
