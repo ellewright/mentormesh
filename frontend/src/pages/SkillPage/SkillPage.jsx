ec33214b0a29ad85b33d58d68797061b48961c6f
Eleanor Wright <elliewright055@gmail.com>
2025-05-23 15:02:41 -0500

    Add link to SkillPage to route to the CategoriesPage

--- frontend/src/pages/SkillPage/SkillPage.jsx
+++ frontend/src/pages/SkillPage/SkillPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import styles from "./SkillPage.module.css"
 import { getSkillById } from "../../api/config"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 export default function SkillPage() {
     const { id } = useParams()
@@ -29,7 +29,11 @@ export default function SkillPage() {
         <div className={styles.container}>
             <div className={styles.header}>
                 <h1 className={styles.title}>{skill.title ? skill.title : ""}</h1>
-                <p className={styles.category}>{skill.category ? `Category: ${skill.category.toUpperCase()}` : ""}</p>
+                <p className={styles.categoryContainer}>
+                    <Link to={`/${skill.category}`} className={styles.category}>
+                        {skill.category ? `${skill.category.toUpperCase()}` : ""}
+                    </Link>
+                </p>
             </div>
             <p>{skill.description ? skill.description : ""}</p>
         </div>
