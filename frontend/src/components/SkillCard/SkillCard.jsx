4337c9633eacde3ac9f4b98068b5cd4b90ccb2ad
Eleanor Wright <elliewright055@gmail.com>
2025-05-23 10:42:16 -0500

    Add link to SkillCard to route to CategoriesPage

--- frontend/src/components/SkillCard/SkillCard.jsx
+++ frontend/src/components/SkillCard/SkillCard.jsx
@@ -4,12 +4,14 @@ import { Link } from "react-router-dom"
 export default function SkillCard({ id, title, description, category }) {
     return (
         <div className={styles.card}>
-            <p className={styles.category}>
-                {category.toUpperCase()}
+            <p className={styles.categoryContainer}>
+                <Link to={`/${category}`} className={styles.category}>
+                    {category.toUpperCase()}
+                </Link>
             </p>
             <div className={styles.titleContainer}>
                 <h1 className={styles.title}>
-                    <Link as="h1" to={`/skill/${id}`}>
+                    <Link to={`/skill/${id}`}>
                         {title}
                     </Link>
                 </h1>
